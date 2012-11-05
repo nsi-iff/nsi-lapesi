2a94615b35951ac7b9cbd4f42c6792599790d3f4
Pedro Henrique Linhares <pedrolmota@gmail.com>
2012-11-05 17:42:07 -0200

    rewind file before reading

--- lib/nsi-lapesi/searcher.rb
+++ lib/nsi-lapesi/searcher.rb
@@ -12,7 +12,12 @@ module NSILapesi
     end
 
     def search_image(image, limit = 5)
-      image_content = (image.class == String)? Base64.decode64(image) : image.read
+      if image.class == String
+        image_content = Base64.decode64(image)
+      else
+        image.seek 0
+        image_content = image.read
+      end
 
       results_xml = @server.call("ImageSearcher.searchImage", XMLRPC::Base64.new(image_content), limit)
       results = XmlSimple.xml_in results_xml
